1ca2e89eaaf6fe9fb108a96695864fc9a2e6e8d8
Tulio Faustino <tulio.fsousa@gmail.com>
2026-08-25 13:55:21 -0300

    separated the implementation of the dfs from the hpp to the cpp file

--- graphs/include/dfs.hpp
+++ graphs/include/dfs.hpp
@@ -2,7 +2,6 @@
 #define DFS_HPP
 
 #include "../include/graph.hpp"
-#include <iostream>
 #include <vector>
 #include <string>
 
@@ -19,78 +18,15 @@ struct dfs_vertex {
     int d;
     int f;
 
-    dfs_vertex(int _key, Colour _colour, int _pi) {
-        key = _key;
-        colour = _colour;
-        pi = _pi;
-        d = 0;
-        f = -1;
-    }
+    dfs_vertex(int _key, Colour _colour = WHITE, int _pi = -1);
 };
 
-inline std::string colour_to_string(Colour c) {
-    switch (c) {
-        case WHITE: return "BRANCO";
-        case GREY:  return "CINZA";
-        case BLACK: return "PRETO";
-        default:    return "DESCONHECIDO";
-    }
-}
+std::string colour_to_string(Colour c);
 
-void dfs_visit(Graph &G, std::vector<dfs_vertex> &v, int idx, int &time) {
-    time++;
-    v[idx].d = time;
-    v[idx].colour = GREY;
+void dfs_visit(Graph &G, std::vector<dfs_vertex> &v, int idx, int &time);
 
-    std::vector<int> adj_v = G.edges(idx);
-    for (size_t i = 0; i < adj_v.size(); i++) {
-        int u = adj_v[i];
-        if (v[u].colour == WHITE) {
-            v[u].pi = idx;
-            dfs_visit(G, v, u, time);
-        }
-    }
+std::vector<dfs_vertex> depth_first_search(Graph &G);
 
-    time++;
-    v[idx].f = time;
-    v[idx].colour = BLACK;
-}
-
-std::vector<dfs_vertex> depth_first_search(Graph &G) {
-    std::vector<dfs_vertex> vertices;
-    for (size_t i = 0; i < G.size(); i++) {
-        vertices.push_back(dfs_vertex(i, WHITE, -1));
-    }
-
-    int time = 0;
-    int flag_connected = 0;
-
-    for (size_t i = 0; i < vertices.size(); i++) {
-        if (vertices[i].colour == WHITE) {
-            dfs_visit(G, vertices, i, time);
-            flag_connected++; // If bigger than 1, the graph is not connected
-        }
-    }
-
-    return vertices;
-}
-
-void print_adjacencies_with_colours(Graph &G, const std::vector<dfs_vertex> &v) {
-    for (size_t u = 0; u < G.size(); ++u) {
-        std::cout << "Vértice " << u 
-                  << " [" << colour_to_string(v[u].colour) 
-                  << ", d: " << v[u].d << ", f: " << v[u].f << "]:\n";
-
-        std::vector<int> adj = G.edges(u);
-        if (adj.empty()) {
-            std::cout << "  -> (sem adjacências)\n";
-        } else {
-            for (int neighbor : adj) {
-                std::cout << "  -> Vizinho " << neighbor 
-                          << " [" << colour_to_string(v[neighbor].colour) << "]\n";
-            }
-        }
-    }
-}
+void print_adjacencies_with_colours(Graph &G, const std::vector<dfs_vertex> &v);
 
 #endif // DFS_HPP
--- graphs/src/dfs.cpp
+++ graphs/src/dfs.cpp
@@ -1,92 +1,14 @@
-/*#include "../include/graph.hpp"
-#include <string>
+#include "../include/dfs.hpp"
+#include <iostream>
 
- Pseudocode for DFS
-
-DFS(G)
-1   for each vertex u in G.V
-2       u.color = WHITE
-3       u.pi = NIL
-4   time = 0
-5   for each vertex u in G.V
-6       if u.color == WHITE
-7           DFS-VISIT(G, u)
-
-
-DFS-VISIT(G, u)
-1   time = time + 1
-2   u.d = time
-3   u.color = GRAY
-4   for each vertex v in G.Adj[u]
-5       if v.color == WHITE
-6           v.pi = u
-7           DFS-VISIT(G, v)
-8   time = time + 1
-9   u.f = time
-10  u.color = BLACK
-
-//
-
-enum Colour {
-    WHITE = 0,
-    GREY = 1,
-    BLACK = 2
-};
-
-struct dfs_vertex{
-    int key;
-    Colour colour; // 0 for white, 1 for grey and 2 for black
-    int pi; // refers to the last visited vertex, -1 for NIL
-    int d;
-    int f;
-
-    dfs_vertex(int _key, Colour _colour, int _pi){
-        key = _key;
-        colour = _colour;
-        pi = _pi;
-        d = 0;
-        f = -1;
-    }
-};
-
-template <typename T>
-void dfs_visit(Graph<T> &G, std::vector<dfs_vertex> &v, int idx, int& time){
-    time++;
-    v[idx].d = *time;
-    v[idx].colour = GRAY;
-    std::vector<int> adj_v = G.edges(idx);
-    for(size_t = i; i < adj_v.size(); i++){
-        if(v[adj_v[i]].colour == WHITE){
-            v[adj_v[i]].pi = idx;
-            dfs_visit(G, v, i, time)
-        }
-    }
-    time++;
-    v[idx].f = *time;
-    v[idx].colour = BLACK;
-
+dfs_vertex::dfs_vertex(int _key, Colour _colour, int _pi) {
+    key = _key;
+    colour = _colour;
+    pi = _pi;
+    d = 0;
+    f = -1;
 }
 
-template <typename T>
-void depth_first_search(Graph<T> &G){
-    std::vector<dfs_vertex> vertices;
-    for(size_t i = 0; i < G.size(); i++){
-        vertices.push_back(dfs_vertex(i, WHITE, -1))
-    }
-
-    int time = 0;
-
-    int flag_connected = 0;
-    for(size_t i = 0; i < vertices.size(); i++){
-        if (vertices[i].colour == WHITE){
-            dfs_visit(G, vertices, i, time);
-            flag_connected++; // If bigger then 1, the graph is not connected
-        }
-    }
-
-};
-
-// AI generated print function:
 std::string colour_to_string(Colour c) {
     switch (c) {
         case WHITE: return "BRANCO";
@@ -96,9 +18,45 @@ std::string colour_to_string(Colour c) {
     }
 }
 
-// Função para imprimir cada vértice, seus vizinhos e as cores correspondentes
-template <typename T>
-void print_adjacencies_with_colours(Graph<T> &G, const std::vector<dfs_vertex> &v) {
+void dfs_visit(Graph &G, std::vector<dfs_vertex> &v, int idx, int &time) {
+    time++;
+    v[idx].d = time;
+    v[idx].colour = GREY;
+
+    std::vector<int> adj_v = G.edges(idx);
+    for (size_t i = 0; i < adj_v.size(); i++) {
+        int u = adj_v[i];
+        if (v[u].colour == WHITE) {
+            v[u].pi = idx;
+            dfs_visit(G, v, u, time);
+        }
+    }
+
+    time++;
+    v[idx].f = time;
+    v[idx].colour = BLACK;
+}
+
+std::vector<dfs_vertex> depth_first_search(Graph &G) {
+    std::vector<dfs_vertex> vertices;
+    for (size_t i = 0; i < G.size(); i++) {
+        vertices.push_back(dfs_vertex(i, WHITE, -1));
+    }
+
+    int time = 0;
+    int flag_connected = 0;
+
+    for (size_t i = 0; i < vertices.size(); i++) {
+        if (vertices[i].colour == WHITE) {
+            dfs_visit(G, vertices, i, time);
+            flag_connected++; // If bigger than 1, the graph is not connected
+        }
+    }
+
+    return vertices;
+}
+
+void print_adjacencies_with_colours(Graph &G, const std::vector<dfs_vertex> &v) {
     for (size_t u = 0; u < G.size(); ++u) {
         std::cout << "Vértice " << u 
                   << " [" << colour_to_string(v[u].colour) 
@@ -115,4 +73,3 @@ void print_adjacencies_with_colours(Graph<T> &G, const std::vector<dfs_vertex> &
         }
     }
 }
-*/
